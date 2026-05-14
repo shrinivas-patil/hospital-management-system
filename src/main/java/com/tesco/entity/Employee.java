9089b842f60845e2ec9547cd683b1f8e038182b1
shrinivas <shriipatil10@gmail.com>
2026-05-14 16:21:43 +0530

    EMP-101 Added employee save API

--- src/main/java/com/tesco/entity/Employee.java
+++ src/main/java/com/tesco/entity/Employee.java
@@ -1,15 +1,21 @@
 package com.tesco.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
-@Entity
+
+
 @Data
+@Entity
 public class Employee {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
+    private String department;
 }
